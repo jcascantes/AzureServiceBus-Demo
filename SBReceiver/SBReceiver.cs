3107c3dff4a951357121f0b8c2c0e5119a505fe1
Jose Luis Cascante <joselcaz@hotmail.com>
2023-05-30 22:32:52 -0600

    moving code to separated class

---
+++ SBReceiver/SBReceiver.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Azure.ServiceBus;
+using SBShared.Models;
+using System.Text;
+using System.Text.Json;
+
+public class SBReceiver
+{
+    const string connectionString = @"[Replace]";
+    const string queueName = "personqueue";
+    IQueueClient queueClient;
+
+    public SBReceiver()
+    {
+        queueClient = new QueueClient(connectionString, queueName);
+
+        var messageHandlerOptions = new MessageHandlerOptions(ExceptionReceivedHandler)
+        {
+            MaxConcurrentCalls = 1,
+            AutoComplete = false
+        };
+
+        queueClient.RegisterMessageHandler(ProcessMessagesAsync, messageHandlerOptions);
+    }
+
+    async Task ProcessMessagesAsync(Message message, CancellationToken token)
+    {
+        var jsonString = Encoding.UTF8.GetString(message.Body);
+        var person = JsonSerializer.Deserialize<PersonModel>(jsonString);
+        Console.WriteLine($"Person received: {person?.FirstName} {person?.LastName}");
+        await queueClient.CompleteAsync(message.SystemProperties.LockToken);
+    }
+
+    Task ExceptionReceivedHandler(ExceptionReceivedEventArgs args)
+    {
+        Console.WriteLine($"MEssage handler exception: {args.Exception}");
+        return Task.CompletedTask;
+    }
+}
